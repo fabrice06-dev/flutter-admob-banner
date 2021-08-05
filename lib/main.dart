d2c515b234cbe5a3dff0c636d0a98806cd37bbf2
unknown <huehanou.houessou@inphb.ci>
2021-08-05 16:19:52 +0000

    Readme file update with a link to the tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+// first
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
